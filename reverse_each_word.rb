dccac1e42222ae0cddc922dc549ea42b5084e5d5
Thor Summerall <thor.summerall@gmail.com>
2020-11-19 01:05:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  sentence_array.each do |reversed_sentence|
-
+  sentence_array.collect do |word|
+      word.reverse
   end
+  sentence_array.join(" ")
 end
